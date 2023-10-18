0d6e07057ba9e76e8996e7420f88ec14756538e9
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-18 19:58:20 -0400

    Add seed comic data

---
+++ src/app/comics-list/comics.ts
@@ -0,0 +1,23 @@
+interface Comics {
+    id: number,
+    title: string,
+    description: string
+}
+
+export const comics: Comics[] = [
+    {
+        id: 3,
+        title: 'MecBros',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!'
+    },
+    {
+        id: 2,
+        title: 'Red Ranger',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!'
+    },
+    {
+        id: 1,
+        title: 'The Tale of Two Sisters',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!'
+    }
+]

fed3c919b5727d97de6de15ad73bc70934b86583
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-18 15:41:53 -0400

    generate comics component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ComicsComponent } from './comics/comics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ComicsComponent
   ],
   imports: [
     BrowserModule,

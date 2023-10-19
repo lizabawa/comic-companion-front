d9edbbf15da7f76f95576a7cb0798fec6651d178
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-18 20:07:26 -0400

    Refactor comic details to loop

--- src/app/comics-list/comics-list.component.ts
+++ src/app/comics-list/comics-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { comics } from './comics';
 
 @Component({
   selector: 'app-comics-list',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./comics-list.component.css']
 })
 export class ComicsListComponent {
-
+  comics = comics
 }
--- src/app/comics-list/comics.ts
+++ src/app/comics-list/comics.ts
@@ -2,22 +2,26 @@ interface Comics {
     id: number,
     title: string,
     description: string
+    url: any
 }
 
 export const comics: Comics[] = [
     {
         id: 3,
         title: 'MecBros',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!',
+        url: ('assets/images/mec-bros.png')
     },
     {
         id: 2,
         title: 'Red Ranger',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!',
+        url: ('assets/images/mec-bros.png')
     },
     {
         id: 1,
         title: 'The Tale of Two Sisters',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!',
+        url: ('assets/images/mec-bros.png')
     }
 ]

7b5b866dc9f317df88f5eadbefed0bace0a84b75
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-22 19:07:58 -0400

    Import WebRequestService to comics list service

--- src/app/comics-list/comics-list.service.ts
+++ src/app/comics-list/comics-list.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { WebRequestService } from '../web-request.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComicsListService {
-  constructor() { }
+  constructor(private webReqService: WebRequestService) { }
 
   seeMore() {
     // send a web request to get more comic titles and descriptions
